6c8a2f60c0c5f36260750622cfb4e9cb450efbf9
CrazyDev <70252473+CracyDev@users.noreply.github.com>
2022-01-15 05:52:14 +0300

    Full

---
+++ crypto-tracker-front/crypto-tracker-front-master/src/constants/Constants.js
@@ -0,0 +1,10 @@
+const Constants = {
+    SERVER_URL: "https://crypto-tracker-backend-app.herokuapp.com",
+    Status : {
+        NONE: 'NONE',
+        REQUEST: 'REQUEST',
+        SUCCESS: 'SUCCESS',
+        FAILURE: 'FAILURE',
+    }
+}
+export default Constants;
